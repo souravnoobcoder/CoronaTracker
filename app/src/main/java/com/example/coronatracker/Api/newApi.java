4a69464c60685bbeedcf0d8a271571c18d01183a
souravnoobcoder <souravrawat77777777@gmail.com>
2021-09-04 15:36:33 +0530

    checking for cache

--- app/src/main/java/com/example/coronatracker/Api/newApi.java
+++ app/src/main/java/com/example/coronatracker/Api/newApi.java
@@ -1,10 +1,39 @@
 package com.example.coronatracker.Api;
 
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import java.io.IOException;
+
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class newApi {
 
+
+    private static final long cacheSize = (1024 * 1024);
+    private static final Cache cache=new Cache(MyApplication.getInstance().getCacheDir(),cacheSize);
+    private static final OkHttpClient client=new OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request = chain.request();
+                    if (MyApplication.hasNetwork())
+                        request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build();
+                    else
+                        request.newBuilder().header("Cache-Control", "public, " +
+                                "only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build();
+                    chain.proceed(request);
+                    return null;
+                }
+            }).build();
 
     private static Retrofit COVID_API_INSTANCE;
     private static Retrofit WORLD;
@@ -27,6 +56,7 @@ public class newApi {
         if (WORLD==null){
             WORLD=new Retrofit.Builder()
                     .baseUrl(WORLD_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
